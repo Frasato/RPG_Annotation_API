027233f5f62f7a55f5abd6993e1516d738192f96
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-22 16:45:33 -0300

    :test_tube: test: Create a user and Check the information

--- src/test/java/com/frasato/rpgannotation/UserModelTest.java
+++ src/test/java/com/frasato/rpgannotation/UserModelTest.java
@@ -1,10 +1,28 @@
 package com.frasato.rpgannotation;
 
+import com.frasato.rpgannotation.models.UserModel;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserModelTest {
 
+    @Test
+    void testUserModelCreation(){
 
+        UserModel user = new UserModel();
+
+        user.setUserId("123");
+        user.setUsername("Tester");
+        user.setEmail("tester@test.com");
+        user.setPassword("000");
+
+        assertEquals("123", user.getUserId());
+        assertEquals("Tester", user.getUsername());
+        assertEquals("tester@test.com", user.getEmail());
+        assertEquals("000", user.getPassword());
+    }
 
 }
