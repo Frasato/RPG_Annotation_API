1a752f14010a906bf23922ab8759f9cb2673883b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-22 15:49:16 -0300

    :sparkles: feat: Create attributes on User Model

--- src/main/java/com/frasato/rpgannotation/models/UserModel.java
+++ src/main/java/com/frasato/rpgannotation/models/UserModel.java
@@ -1,9 +1,18 @@
 package com.frasato.rpgannotation.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userId;
+    @Column(nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
 }
