ed5a9be6192c8885270e3cbf0d91c50ef04b0f29
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-22 16:58:15 -0300

    :test_tube: test: Finish the tests

--- src/test/java/com/frasato/rpgannotation/PostModelTest.java
+++ src/test/java/com/frasato/rpgannotation/PostModelTest.java
@@ -1,7 +1,21 @@
 package com.frasato.rpgannotation;
 
+import com.frasato.rpgannotation.models.PostModel;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PostModelTest {
+
+    @Test
+    void testCreatePostModel(){
+        PostModel post = new PostModel();
+
+        post.setPostId("post1");
+        post.setTitle("Post 1");
+        post.setContent("Post Test 1");
+
+
+    }
+
 }
