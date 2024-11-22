345b61ac0b43e29bd22567d350252ebd2ad4d040
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-22 16:15:20 -0300

    :sparkles: feat: Create Getters and Setters

--- src/main/java/com/frasato/rpgannotation/models/PostModel.java
+++ src/main/java/com/frasato/rpgannotation/models/PostModel.java
@@ -19,4 +19,55 @@ public class PostModel {
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "creator_user_id")
     private UserModel userId;
+
+    public PostModel() {
+    }
+
+    public PostModel(String postId, String title, String content, Integer like, UserModel userId) {
+        this.postId = postId;
+        this.title = title;
+        this.content = content;
+        this.like = like;
+        this.userId = userId;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLike() {
+        return like;
+    }
+
+    public void setLike(Integer like) {
+        this.like = like;
+    }
+
+    public UserModel getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UserModel userId) {
+        this.userId = userId;
+    }
 }
