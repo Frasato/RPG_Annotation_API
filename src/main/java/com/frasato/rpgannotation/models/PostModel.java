97eebc917a12aa464d54dd94f2b3f87252284a64
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-22 15:57:52 -0300

    :sparkles: feat: Create Attributes

--- src/main/java/com/frasato/rpgannotation/models/PostModel.java
+++ src/main/java/com/frasato/rpgannotation/models/PostModel.java
@@ -1,9 +1,19 @@
 package com.frasato.rpgannotation.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "posts")
 public class PostModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String postId;
+
+    private String title;
+    private String content;
+    private Integer like;
+
+    @ManyToOne
+    private UserModel userId;
 }
