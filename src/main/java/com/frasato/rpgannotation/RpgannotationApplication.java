a245467e602beb970b7ffa9371210f8492d5ae15
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-22 14:26:45 -0300

    Create Project

---
+++ src/main/java/com/frasato/rpgannotation/RpgannotationApplication.java
@@ -0,0 +1,13 @@
+package com.frasato.rpgannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgannotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgannotationApplication.class, args);
+	}
+
+}
