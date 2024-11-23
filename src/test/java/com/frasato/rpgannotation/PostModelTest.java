38afaaa8b2d537ae2cfcf0ec7c0d21365e1a999e
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-23 11:22:18 -0300

    :white_check_mark: test: Success test creation post

--- src/test/java/com/frasato/rpgannotation/PostModelTest.java
+++ src/test/java/com/frasato/rpgannotation/PostModelTest.java
@@ -1,21 +1,34 @@
 package com.frasato.rpgannotation;
 
 import com.frasato.rpgannotation.models.PostModel;
+import com.frasato.rpgannotation.models.UserModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class PostModelTest {
 
     @Test
     void testCreatePostModel(){
-        PostModel post = new PostModel();
+        UserModel user = new UserModel();
+        user.setUserId("1");
+        user.setUsername("Tester");
+        user.setEmail("test@test.com");
+        user.setPassword("123");
 
+        PostModel post = new PostModel();
         post.setPostId("post1");
         post.setTitle("Post 1");
         post.setContent("Post Test 1");
+        post.setLike(1);
+        post.setUser(user);
 
-
+        assertEquals("post1", post.getPostId());
+        assertEquals("Post 1", post.getTitle());
+        assertEquals("Post Test 1", post.getContent());
+        assertEquals(1, post.getLike());
+        assertEquals(user, post.getUser());
     }
 
 }
