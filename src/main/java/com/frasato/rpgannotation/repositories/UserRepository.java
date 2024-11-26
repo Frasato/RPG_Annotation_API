f5f56bf99a5adebecc5b5edc5115ec7fcf0d99cc
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-26 11:46:08 -0300

    :sparkles: feat: Add find by email

--- src/main/java/com/frasato/rpgannotation/repositories/UserRepository.java
+++ src/main/java/com/frasato/rpgannotation/repositories/UserRepository.java
@@ -4,5 +4,11 @@ import com.frasato.rpgannotation.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, String> {}
+public interface UserRepository extends JpaRepository<UserModel, String> {
+
+    Optional<UserModel> findByEmail(String email);
+
+}
