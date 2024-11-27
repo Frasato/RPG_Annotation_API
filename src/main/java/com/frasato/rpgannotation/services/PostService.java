486e5d8be2e916b92d272f1748cdcc6a75723c4d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-27 16:09:25 -0300

    :sparkles: feat: Create function to add post

---
+++ src/main/java/com/frasato/rpgannotation/services/PostService.java
@@ -0,0 +1,30 @@
+package com.frasato.rpgannotation.services;
+
+import com.frasato.rpgannotation.models.PostModel;
+import com.frasato.rpgannotation.models.UserModel;
+import com.frasato.rpgannotation.repositories.PostRepository;
+import com.frasato.rpgannotation.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createPost(String userId, String title, String content){
+        UserModel user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("ERROR: User do not exist! " + userId));
+        PostModel postModel = new PostModel();
+
+        postModel.setUser(user);
+        postModel.setTitle(title);
+        postModel.setContent(content);
+
+        postRepository.save(postModel);
+
+    }
+
+}
