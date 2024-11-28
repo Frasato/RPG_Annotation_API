ceab2326c579483c8849ea4284661249be42f598
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-28 09:54:24 -0300

    :sparkles: feat: posts by user id query

--- src/main/java/com/frasato/rpgannotation/repositories/PostRepository.java
+++ src/main/java/com/frasato/rpgannotation/repositories/PostRepository.java
@@ -2,7 +2,16 @@ package com.frasato.rpgannotation.repositories;
 
 import com.frasato.rpgannotation.models.PostModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PostRepository extends JpaRepository<PostModel, String> {}
+public interface PostRepository extends JpaRepository<PostModel, String> {
+
+    @Query(value = "SELECT * FROM posts WHERE creator_user_id%:id%", nativeQuery = true)
+    List<PostModel> findPostsByUser(@Param("id") String id);
+
+}
